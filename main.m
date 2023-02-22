922033beccd05df49a25546e0c6c43d5c9470489
Christian Ibarra <106888385+Weeks2@users.noreply.github.com>
2023-02-21 21:28:02 -0600

    Update main.m

--- main.m
+++ main.m
@@ -1,10 +1,10 @@
-function [resultado] = sumar(a,b)
-    resultado = a + b
-end 
-
-function  main()
+function resultado = main()
     a = input("Cual es el valor de a:")
     b = input("Cual es el valor de b:")
     resultado = sumar(a,b);
+end 
+
+function [resultado] = sumar(a,b)
+    resultado = a + b;
 end
 
